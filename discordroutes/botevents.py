7c9effc6435db6d7b27a66c4b3822ec1ba9e8bce
TaruDesigns <taruelec@gmail.com>
2024-07-13 18:37:31 +0200

    More fixes for the breadstats top thingie

--- discordroutes/botevents.py
+++ discordroutes/botevents.py
@@ -5,13 +5,14 @@ import discord
 from loguru import logger
 
 from db.models import (
+    OrderBy,
     get_minmax_roundness_byuserid,
     get_minmax_roundness_leaderboard,
     select_user_info,
     upsert_message_discordinfo,
     upsert_user_info,
 )
-
+from discordroutes import bread as breadroute
 
 # This example requires the 'message_content' intent.
 intents = discord.Intents.default()
@@ -27,23 +28,30 @@ async def breadstats_handler(message: discord.Message, args: list[str]):
         message (discord.Message): Discord message
         args (list[str]): message content arguments
     """
-    if args[1] == "--self":
+    if len(args) < 2:
+        await message.channel.send(content="Not enough arguments", reference=message)
+    elif args[1] == "--self":
         # Return results (top 1) for current user
-        results = get_minmax_roundness_byuserid(message.author.id)
+        results_min = get_minmax_roundness_byuserid(
+            message.author.id, orderby=OrderBy.ASC.value
+        )
         min_roundness_percent = (
-            round(results["min_roundness"] * 100, 2)
-            if results["min_roundness"] is not None
+            round(results_min["roundness"] * 100, 2)
+            if results_min["roundness"] is not None
             else 0
         )
+        results_max = get_minmax_roundness_byuserid(
+            message.author.id, orderby=OrderBy.DES.value
+        )
         max_roundness_percent = (
-            round(results["max_roundness"] * 100, 2)
-            if results["max_roundness"] is not None
+            round(results_max["roundness"] * 100, 2)
+            if results_max["roundness"] is not None
             else 0
         )
         reply_content = f"""
                             Hello {message.author.name}:
-                            Min roundness:  {min_roundness_percent:.2f}% on message: {results["min_roundness_url"]},
-                            Max roundness {max_roundness_percent:.2f}% on message: {results["max_roundness_url"]}
+                            Min roundness:  {min_roundness_percent:.2f}% on message: {results_min["roundness_url"]},
+                            Max roundness {max_roundness_percent:.2f}% on message: {results_max["roundness_url"]}
                             """
         await message.channel.send(content=reply_content, reference=message)
     elif args[1] == "--top":
@@ -57,30 +65,32 @@ async def breadstats_handler(message: discord.Message, args: list[str]):
                     f" (You're asking too much, nobody has seen a top {limit} ever)"
                 )
         except Exception as e:
-            logger.warn(e)
+            logger.warning(e)
             limit = 3
             append_to_limit = " (You didn't enter a valid number. Shame on you)"
-        results = get_minmax_roundness_leaderboard(limit)
+        # Query DB and get results
+        results_max = get_minmax_roundness_leaderboard(limit, orderby=OrderBy.DES.value)
+        results_min = get_minmax_roundness_leaderboard(limit, orderby=OrderBy.ASC.value)
         # Generate message part for top X
         reply_content_max = f"Top {limit}{append_to_limit}:"
-        for idx, val in enumerate(results["max_roundness"]):
-            ogmessage: discord.Message = await get_message_by_id(
-                val["guild_id"], val["channel_id"], val["ogmessage_id"]
-            )
+        for idx, val in enumerate(results_max):
             roundness_percent = (
                 round(val["roundness"] * 100, 2) if val["roundness"] is not None else 0
             )
-            reply_content_max = f"""{reply_content_max}\n #{idx + 1}: {ogmessage.author.name} with {roundness_percent:.2f}% on message {val["jump_url"]}"""
+            user_info = select_user_info(val["author_id"])
+            if user_info is None:
+                user_info = {"author_name": "unknown"}
+            reply_content_max = f"""{reply_content_max}\n #{idx + 1}: {user_info["author_name"]} with {roundness_percent:.2f}% on message {val["jump_url"]}"""
         # Generate message part for worst X
         reply_content_min = "Worst 3:"
-        for idx, val in enumerate(results["min_roundness"]):
-            ogmessage: discord.Message = await get_message_by_id(
-                val["guild_id"], val["channel_id"], val["ogmessage_id"]
-            )
+        for idx, val in enumerate(results_min):
+            user_info = select_user_info(val["author_id"])
+            if user_info is None:
+                user_info = {"author_name": "unknown"}
             roundness_percent = (
                 round(val["roundness"] * 100, 2) if val["roundness"] is not None else 0
             )
-            reply_content_min = f"""{reply_content_min}\n #{idx + 1}: {ogmessage.author.name} with {roundness_percent:.2f}% on message {val["jump_url"]}"""
+            reply_content_min = f"""{reply_content_min}\n #{idx + 1}: {user_info["author_name"]} with {roundness_percent:.2f}% on message {val["jump_url"]}"""
 
         reply_content = f"{reply_content_max}\n{reply_content_min}"
         await message.channel.send(content=reply_content, reference=message)
@@ -106,18 +116,19 @@ async def breadinference_handler(message: discord.Message, args: list[str]):
     # Check Bread Candidate Message
     try:
         if await breadroute.check_bread_message(message=message):
-            sentmessage = await breadroute.send_bread_message(
+            sentmessages = await breadroute.send_bread_message(
                 message=message, overrideconfidence=False
             )
             # Store data in DB
-            upsert_message_discordinfo(
-                ogmessage_id=message.id,
-                replymessage_jump_url=sentmessage.jump_url,
-                replymessage_id=sentmessage.id,
-                author_id=message.author.id,
-                channel_id=message.channel.id,
-                guild_id=message.guild.id,
-            )
+            for sentmessage in sentmessages:
+                upsert_message_discordinfo(
+                    ogmessage_id=message.id,
+                    replymessage_jump_url=sentmessage.jump_url,
+                    replymessage_id=sentmessage.id,
+                    author_id=message.author.id,
+                    channel_id=message.channel.id,
+                    guild_id=message.guild.id,
+                )
         elif await breadroute.check_areyousure_message(
             message=message, botuser=bot.user
         ):
@@ -131,18 +142,19 @@ async def breadinference_handler(message: discord.Message, args: list[str]):
                 message_id=ogmessageref.message_id,
             )
             # TODO double check that it the og message is a bread message?
-            sentmessage = await breadroute.send_bread_message(
+            sentmessages = await breadroute.send_bread_message(
                 message=ogmessage, overrideconfidence=True
             )
             # Store data in DB
-            upsert_message_discordinfo(
-                ogmessage_id=ogmessage.id,
-                replymessage_jump_url=sentmessage.jump_url,
-                replymessage_id=sentmessage.id,
-                author_id=message.author.id,
-                channel_id=message.channel.id,
-                guild_id=message.guild.id,
-            )
+            for sentmessage in sentmessages:
+                upsert_message_discordinfo(
+                    ogmessage_id=ogmessage.id,
+                    replymessage_jump_url=sentmessage.jump_url,
+                    replymessage_id=sentmessage.id,
+                    author_id=message.author.id,
+                    channel_id=message.channel.id,
+                    guild_id=message.guild.id,
+                )
 
     except Exception as e:
         logger.error(e)
@@ -179,11 +191,15 @@ async def on_message(message: discord.Message):
         return
     message_args = shlex.split(message.content.strip())
     func_args = (message, message_args)
-    if message_args[0].startswith("$") and message_args[0] in mapping_functions.keys():
+    if (
+        message_args
+        and message_args[0].startswith("$")
+        and message_args[0] in mapping_functions.keys()
+    ):
         await mapping_functions[message_args[0]](*func_args)
     else:
         # Default - assume it was a bread message and scan it anyway
-        breadinference_handler(message=message, message_args=message_args)
+        await breadinference_handler(message=message, args=message_args)
 
 
 async def get_message_by_id(guild_id: int, channel_id: int, message_id: int):
